dd6a56cf1e47dda364c08cdfd2724de18190b686
yjiang479 <yjiang479@gatech.edu>
2024-08-03 09:18:47 -0700

    Initial Commit

---
+++ Recipe Browse/Recipe_BrowseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_BrowseApp.swift
+//  Recipe Browse
+//
+//  Created by Yajing Jiang on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_BrowseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
